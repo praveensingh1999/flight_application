c3cea2fc81ead037d1afa72046f00da063f7912f
praveensingh1999 <praveen969500@gmail.com>
2024-01-17 23:08:20 +0530

    changes in code

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -1,4 +1,4 @@
-const {statusCodes, StatusCodes} = require('http-status-codes');
+const { StatusCodes} = require('http-status-codes');
 const {AirplaneRepository}=require('../repositories');
 const AppError = require('../utils/error/app-error');
 const airplaneRepository = new AirplaneRepository();
@@ -12,10 +12,10 @@ async function createAirplane(data){
             error.errors.forEach(err => {
                 explanation.push(err.message);
             });
-            throw new AppError(explanation,statusCodes.BAD_REQUEST);
+            throw new AppError(explanation,StatusCodes.BAD_REQUEST);
 
         }
-        throw new AppError('Cannot create anew Airplane object', statusCodes.INTERNAL_SERVER_ERROR);
+        throw new AppError('Cannot create anew Airplane object', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 
 }
@@ -25,7 +25,7 @@ async function getAirplanes(){
         const airplanes = await airplaneRepository.getAll();
         return airplanes;
     } catch (error) {
-        throw new AppError('Cannot fetch data of all the airplanes' , statusCodes.INTERNAL_SERVER_ERROR);
+        throw new AppError('Cannot fetch data of all the airplanes' , StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
 
