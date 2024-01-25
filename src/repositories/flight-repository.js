1bd1d54db35bff8014966005ffb25e65f1ec4a28
praveensingh1999 <praveen969500@gmail.com>
2024-01-26 00:38:21 +0530

    Added transaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -49,14 +49,30 @@ class FlightRepository extends CrudRepository {
         return response;
     }
     async updateRemainingSeats(flightId, seats, dec = true){
-        await db.sequelize.query(addRowLockOnFlights(flightId));
-        const flight = await Flight.findByPk(flightId);
-        if(dec) {
-            await flight.decrement('totalSeats', {by: seats});
-        } else {
-            await flight.increment('totalSeats', {by: seats});
+        // await db.sequelize.query(addRowLockOnFlights(flightId));
+        // const flight = await Flight.findByPk(flightId);
+        // if(dec) {
+        //     await flight.decrement('totalSeats', {by: seats});
+        // } else {
+        //     await flight.increment('totalSeats', {by: seats});
+        // }
+        // return flight;
+
+        const transaction = await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if(+dec) {
+                await flight.decrement('totalSeats', {by: seats}, {transaction: transaction});
+            } else {
+                await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            await transaction.commit();
+            return flight;
+        } catch(error) {
+            await transaction.rollback();
+            throw error;
         }
-        return flight;
     }
 }
 module.exports = FlightRepository;   
